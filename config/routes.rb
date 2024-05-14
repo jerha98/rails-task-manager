9e3a8071ee1a24bb0b5f451745750e84a22c30e2
Jeremy Hanhoff <jhanhoff@outlook.de>
2024-05-14 16:55:36 +0200

    Implemented the Reading, Creating, Updating Methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new"
+
+  get "tasks/:id", to: "tasks#show", as: :task
+
+  get "tasks/:id/edit", to: "tasks#edit" , as: :edit_task
+
+  patch "tasks/:id", to: "tasks#update"
+
+  post "tasks", to: "tasks#create"
+
+  root to: "tasks#index"
 end
